b83219e90370f14a134b2b8f88d7af32fe6269e6
akbarmaulanad22 <zulfikarwxz22@gmail.com>
2024-01-07 15:53:16 +0700

    finish

---
+++ app.js
@@ -0,0 +1,151 @@
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+const { default: helmet } = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.8wuy9ef.mongodb.net/${process.env.MONGODB_DB_DEFAULT}`;
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
+const csrfProtection = csrf();
+const sk = fs.readFileSync('server.key');
+const cert = fs.readFileSync('server.cert');
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  { flags: 'a' }
+);
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     useDefaults: true,
+//     directives: {
+//       'default-src': ["'self'"],
+//       'script-src': ["'self'", "'unsafe-inline'", "'unsafe-hashes'"],
+//       'script-src': ["'self'", "'unsafe-inline'", 'js.stripe.com'],
+//       'style-src': ["'self'", "'unsafe-inline'", 'fonts.googleapis.com'],
+//       'frame-src': ["'self'", 'js.stripe.com'],
+//       'font-src': ["'self'", 'fonts.googleapis.com', 'fonts.gstatic.com'],
+//     },
+//   })
+// );
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get('/500', errorController.get500);
+
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  console.log(error);
+
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn,
+  });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    https
+      .createServer({ key: sk, cert: cert }, app)
+      .listen(process.env.PORT || 3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ public/js/admin.js
@@ -0,0 +1,27 @@
+document
+  .getElementById('deleteButton')
+  ?.addEventListener('click', function (event) {
+    const btn = event.target; // Get the button that was clicked
+    const prodId = btn.parentNode.querySelector('[name=productId]').value;
+    const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+
+    const productElement = btn.closest('article');
+
+    fetch('/admin/product/' + prodId, {
+      method: 'DELETE',
+      headers: {
+        'csrf-token': csrf,
+      },
+    })
+      .then((result) => {
+        return result.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // productElement.remove(); not supported in Internet Explorer
+        productElement.parentNode.removeChild(productElement);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
